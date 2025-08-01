468e197d1f3d3acd08233c2ba28c6008df7e9b78
yadnesh-thorat <yadneshwarthorat@gmail.com>
2025-08-01 13:20:48 +0530

    New

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+//Day one 1 Aug 2025
+
+class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//    Introduction of Variable in Java
+//        int for=13;
+//        int $num=12;
+//        String myName="Yadnesh Thorat from BE IT 2026 Batch";
+//        System.out.println(myName);
+//        Primitive Data Type: These are the basic building blocks that store simple values directly in memory.
+//        Non-Primitive Data Types: These are reference types that store memory addresses of objects.
+//        byte a = 10;
+//        System.out.println(a);
+//        short s = 100;
+//        System.out.println(s);
+//        int x = 1000;
+//        System.out.println(x);
+//        long l = 10000L;
+//        System.out.println(l);
+//        float f = 5.5f;
+//        System.out.println(f);
+//        double d = 99.99;
+//        System.out.println(d);
+//        char c = 'A';
+//        System.out.println(c);
+//        boolean b = true;
+//        System.out.println(b);
+//        char ch=65;
+//        System.out.println(ch);
+// Output :10
+//100
+//1000
+//10000
+//5.5
+//99.99
+//A
+//true
+//A
 
-            System.out.println("Hello World");
-        }
+//        byte	8 bits (1 byte)	    -128 to 127
+//        short 16 bits (2 bytes)	-32,768 to 32,767
+//        int	32 bits (4 bytes)	-2,147,483,648 to 2,147,483,647
+//        long	64 bits (8 bytes)	-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807L
     }
 }
